7f9ebbd11c2345576832699433c9d9c99d8a4a2c
raju <rajusasanksai@gmail.com>
2021-05-26 10:07:25 +0530

    final commit

---
+++ src/test/java/mytest/TempClass.java
@@ -0,0 +1,14 @@
+package mytest;
+
+import org.testng.annotations.Test;
+
+public class TempClass {
+
+	@Test
+	public  void myTest() {
+		// TODO Auto-generated method stub
+		System.out.println("New Class has been added");
+
+	}
+
+}
